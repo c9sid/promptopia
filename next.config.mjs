5480cde6875dcb1b3c8a9c2a013f8a0086a56466
c9sid <kumarsiddharth7666@gmail.com>
2024-03-08 17:31:04 +0530

    config file changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    typescript:{ignoreBuildErrors: true},
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
+  experimental: {
+    // appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      },
+    ]
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
     }
+    return config
   }
-  
-  export default nextConfig
+}
+
+export default nextConfig
