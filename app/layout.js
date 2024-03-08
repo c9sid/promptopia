6bc43a24079e5509e94525c58176432d033dfd8c
c9sid <kumarsiddharth7666@gmail.com>
2024-03-08 18:52:18 +0530

    final change

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/assets/images/logo.svg" sizes="any" />
       <body>
         <Provider>
           <div className="main">
